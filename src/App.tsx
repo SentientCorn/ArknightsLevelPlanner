d9f58157137251be6a11c8562860e1dfa988f80a
SentientCorn <radityanarotama0@gmail.com>
2025-05-20 15:42:38 +0700

    initial

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './styles.css'
+function App() {
+
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState('')
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <div>
+        <button onClick={() => setCount(count + 1)}>
+          Count is: {count}
+        </button>
+      </div>
+      <div>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Type something..."
+        />
+        <p>You typed: {text}</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
